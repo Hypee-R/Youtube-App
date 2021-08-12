f4eb244ee87f3b13889ab718c4eb5f1efa4df0b5
Luis Antonio Altamirano Sánchez <luis.altamirano@people-media.com.mx>
2021-08-12 15:01:26 -0500

    Mostrando videos en cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../../services/youtube.service';
+import { Video } from '../../models/youtube.models';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  videos: Video[] = [];
+  constructor(
+    private youtubeService: YoutubeService
+  ) { }
 
   ngOnInit(): void {
+
+    this.youtubeService.getVideos()
+        .subscribe(resp => {
+          this.videos.push(...resp);
+        })
+
   }
 
 }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { YoutubeResponse } from '../models/youtube.models';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  private youtubeURL = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyCBxKhSv74AIImQbT5Ic0CMXys5UChmmEo';
+  private playList = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getVideos(){
+
+    const params = new HttpParams()
+        .set('key', this.apiKey)
+        .set('part', 'snippet')
+        .set('maxResults', '10')
+        .set('playlistId', this.playList);
+
+    return this.http.get<YoutubeResponse>(`${this.youtubeURL}/playlistItems`, { params: params })
+                .pipe(
+                  map( resp =>{
+                    this.nextPageToken = resp.nextPageToken;
+                    return resp.items
+                  }),
+                  map(items => items.map(video => video.snippet))
+                );
+  }
+
 }
